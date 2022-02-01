326b6231e904fd7e387e7be71c13e4d26d7a1a5b
Bora Lee <bbora1119@gmail.com>
2022-02-01 11:56:33 -0500

    start server side query and mutation, update concurrently and modify graphql version

---
+++ client/src/utils/mutation.js
@@ -0,0 +1,41 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookId: String!, $authors: [String]!, $description: String!, $title: String!) {
+    saveBook(bookId: $bookId, authors: $[authors], description: $description, title: $title) {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+ }
+`;
